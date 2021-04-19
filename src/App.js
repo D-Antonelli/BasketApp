e0b2286610adbbca02a61b02250d79f34116e1e5
Derya Antonelli <derya.antonelli@gmail.com>
2021-04-19 16:07:29 +0100

    Render cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+import Item from "./components/item";
+
+const App = () => {
+  const [ totalCost, setTotalCost ] = useState(0);
+  const [ itemTotal, setItemTotal ] = useState(0);
+
+  const [products] = useState([
+    {
+      name: 'Mountain Dew',
+      cost: '$9.99',
+    },
+
+    {
+      name: 'Desperados',
+      cost: '$15.50'
+    },
+    {
+      name: 'Jack Daniels',
+      cost: '$13.40'
+    }
+  ]);
+
+  useEffect(() => {
+    setTotalCost(totalCost + itemTotal);
+  }, [itemTotal])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="basket" data-testid="basket">
+        <div className="items">
+          {products.map((product, index) => (
+            <Item
+              key={index}
+              name={product.name}
+              sendData={setItemTotal}
+              price={product.cost.replace("$", "")}
+            ></Item>
+          ))}
+        </div>
+        <div className="totals">
+          <h3>{totalCost}</h3>
+          <div className="totals-btn-group">
+            <button>clear</button>
+            <button>check out</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
