4c58c1027645b15129d4e122f782c7acb716cd45
Derya Antonelli <derya.antonelli@gmail.com>
2021-04-19 19:12:32 +0100

    Add functionality to reset items

--- src/App.js
+++ src/App.js
@@ -4,36 +4,42 @@ import "./App.css";
 import Item from "./components/item";
 
 const App = () => {
-  const [ totalCost, setTotalCost ] = useState(0);
- 
+  const [totalCost, setTotalCost] = useState(0);
+
   const [products, setProducts] = useState([
     {
-      name: 'Mountain Dew',
-      cost: '$9.99',
-      quantity: 0
+      name: "Mountain Dew",
+      cost: "$9.99",
+      quantity: 0,
     },
 
     {
-      name: 'Desperados',
-      cost: '$15.50',
-      quantity: 0
+      name: "Desperados",
+      cost: "$15.50",
+      quantity: 0,
     },
     {
-      name: 'Jack Daniels',
-      cost: '$13.40',
-      quantity: 0
-    }
+      name: "Jack Daniels",
+      cost: "$13.40",
+      quantity: 0,
+    },
   ]);
 
+  const resetQuantity = () => {
+    products.forEach((item) => item.quantity = 0);
+    setProducts([...products]);
+  };
 
-  const getTotalCost = items => {
-     return items.reduce((prev, curr) => prev + curr.cost.replace("$", "") * curr.quantity, 0);
-  }
+  const getTotalCost = (items) => {
+    return items.reduce(
+      (prev, curr) => prev + curr.cost.replace("$", "") * curr.quantity,
+      0
+    );
+  };
 
   useEffect(() => {
-    setTotalCost(getTotalCost(products))
+    setTotalCost(getTotalCost(products));
   }, [products]);
-
 
   return (
     <div className="App">
@@ -43,17 +49,26 @@ const App = () => {
             <Item
               key={index}
               name={product.name}
-              sendQty={qty => {products[index].quantity = qty;
-                              setProducts([...products])}}
+              sendQty={(qty) => {
+                products[index].quantity = qty;
+                setProducts([...products]);
+              }}
               price={product.cost.replace("$", "")}
+              quantity={product.quantity}
             ></Item>
           ))}
         </div>
         <div className="totals">
           <h3>${totalCost}</h3>
           <div className="totals-btn-group">
-            <button>clear</button>
-            <button>check out</button>
+            <button
+              onClick={() => {
+                resetQuantity(); 
+              }}
+            >
+              clear
+            </button>
+            <button>checkout</button>
           </div>
         </div>
       </div>
