6e9e27332d6472dd5b96358f34eba0b4cf1ab757
Derya Antonelli <derya.antonelli@gmail.com>
2021-04-19 12:24:43 +0100

    setup test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import "@testing-library/jest-dom";
+
+it('renders welcome message', () => {
+    render(<App/>);
+    expect(screen.getByText('Hello Basket')).toBeInTheDocument();
+});
