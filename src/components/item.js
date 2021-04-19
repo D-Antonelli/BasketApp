e06c2da5b48e47788836eba51ab132b76bc3af09
Derya Antonelli <derya.antonelli@gmail.com>
2021-04-19 16:06:37 +0100

    Add cart item component

---
+++ src/components/item.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const Item = ({ name, price, sendData}) => {
+    const [ totalCost, setTotalCost ] = useState(0);
+    const [ qty, setQty ] = useState(0);
+
+    const onQtyChange = (e) => {
+        setQty(e.target.value);
+        
+    }
+
+    useEffect(() => {
+        setTotalCost(qty * price);
+        sendData(qty * price);
+    }, [qty]);
+
+
+    return (
+        <div className="item">
+        <tr className="itemRow">
+        <td>
+          <h3>{name}</h3>  
+        </td>
+        <td>
+         <input className="changeQuantity" size="2" maxLength="3" onChange={onQtyChange} value={qty}></input>   
+        </td>
+        <td className="cost">
+          <h4>${totalCost}</h4>  
+        </td>
+        <td>
+            <button>x</button>
+        </td>
+        </tr>
+        </div>
+        
+    )
+}
+
+
+export default Item;
