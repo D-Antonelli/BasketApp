aae3a586498ba1f71f169be1cb84757d8d3c4473
Derya Antonelli <derya.antonelli@gmail.com>
2021-04-20 10:12:24 +0100

    Use tbody wrapper to fix console error

--- src/components/item.js
+++ src/components/item.js
@@ -17,26 +17,29 @@ const Item = ({ name, price, sendQty, quantity}) => {
 
   return (
     <div className="item h-16 flex items-center border-b-2">
-      <tr className="itemRow">
-        <td className="w-96">
-          <h3 className="font-bold">{name}</h3>
-        </td>
-        <td className="w-32">
-          <input
-            className="changeQuantity capitalize border rounded h-8 text-center"
-            size="2"
-            maxLength="3"
-            onChange={onQtyChange}
-            value={quantity}
-          ></input>
-        </td>
-        <td className="cost w-32">
-          <h4>${totalCost}</h4>
-        </td>
-        <td>
-          <button>x</button>
-        </td>
-      </tr>
+      <tbody>
+        <tr className="itemRow">
+          <td className="w-96">
+            <h3 className="font-bold">{name}</h3>
+          </td>
+          <td className="w-32">
+            <input
+              className="changeQuantity capitalize border rounded h-8 text-center"
+              size="2"
+              maxLength="3"
+              onChange={onQtyChange}
+              value={quantity}
+              data-testid="inputTest"
+            ></input>
+          </td>
+          <td className="cost w-32">
+            <h4 data-testid="costTest">${totalCost}</h4>
+          </td>
+          <td>
+            <button>x</button>
+          </td>
+        </tr>
+      </tbody>
     </div>
   );
 };
